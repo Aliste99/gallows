cd1289c7ffa195c36baff5d79f17bf00da2d674b
Aliste99 <alechka9906@gmail.com>
2017-04-11 19:32:22 +0600

    Implement controller logic

--- app/src/main/java/com/example/thirtyseven/xogame/HangManController.java
+++ app/src/main/java/com/example/thirtyseven/xogame/HangManController.java
@@ -6,17 +6,37 @@ package com.example.thirtyseven.xogame;
 
 class HangManController {
 
-    private String word;
+    private HangManModel model;
 
     public HangManController(HangManModel hangManModel) {
-
+        model = hangManModel;
     }
 
     public void setWord(String word) {
-        this.word = word;
+        model.setWord(word);
     }
 
     public void acceptGuess(String letter) {
+        if (model.getWord().contains(letter)){
+            showLetter(letter);
+        } else model.setGallowPart(model.getGallowPart() + 1);
+    }
 
+    private void showLetter(String letter) {
+        model.setRiddle(findLetters(letter, model.getRiddle(), model.getWord()));
+    }
+
+    private String findLetters(char letter, String riddle, String word)
+    {
+        String updatedRiddle = "";
+        int i = 0;
+        char[] riddleArray = riddle.toCharArray();
+        char[] wordArray = word.toCharArray();
+        for (char symbol: wordArray) {
+            if(symbol == letter) riddleArray[i]=letter;
+            i++;
+            updatedRiddle = String.copyValueOf(riddleArray);
+        }
+        return updatedRiddle;
     }
 }
